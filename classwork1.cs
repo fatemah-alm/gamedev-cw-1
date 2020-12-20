256fd27123da6458d0a4b6668cb9229c711884a1
fatemah-alm <fatemah.alm@hotmail.com>
2020-12-20 20:20:36 +0300

    changed

---
+++ classwork1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace cw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+      
+        {
+            string Charachtername = "Fatima";
+            int age = 24;
+            float speed = 99.9f;
+            string superpower1 = "invisibility";
+            string superpower2 = "controlling thunder";
+            Console.WriteLine("my name is {0} and my superpowers are {1} and {2}, i am {3} years old and my speed is {4}", Charachtername, superpower1, superpower2, age, speed);
+        }
+    }
+}
